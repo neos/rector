4b97fd281573b2f593c026b1bdd931da0cbce5e6
3M5\heinig <robert.heinig@3m5.de>
2022-10-07 16:09:55 +0200

    FEATURE: add rectors NodeIsHiddenInIndexRector and FusionNodeHiddenInIndexRector

---
+++ src/ContentRepository90/Rules/NodeIsHiddenInIndexRector.php
@@ -0,0 +1,53 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Neos\Rector\ContentRepository90\Rules;
+
+use Neos\Rector\Utility\CodeSampleLoader;
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\Collector\NodesToAddCollector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class NodeIsHiddenInIndexRector extends AbstractRector
+{
+    use AllTraits;
+
+    public function __construct(
+        private readonly NodesToAddCollector $nodesToAddCollector
+    )
+    {
+    }
+
+    public function getRuleDefinition() : RuleDefinition
+    {
+        return CodeSampleLoader::fromFile('"NodeInterface::isHiddenInIndex()" will be rewritten', __CLASS__);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes() : array
+    {
+        return [MethodCall::class];
+    }
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node) : ?Node
+    {
+        assert($node instanceof MethodCall);
+
+        if (!$this->isObjectType($node->var, new ObjectType(\Neos\ContentRepository\Core\Projection\ContentGraph\Node::class))) {
+            return null;
+        }
+        if (!$this->isName($node->name, 'isHiddenInIndex')) {
+            return null;
+        }
+
+        return $this->nodeFactory->createMethodCall($node->var, 'getProperty', ['_hiddenInIndex']);
+    }
+}
