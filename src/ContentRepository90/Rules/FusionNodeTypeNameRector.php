3ac63a2aa8ce8adefc06d551585243d00c6c88be
Denny Lubitz <lubitz@vivomedia.de>
2023-09-29 18:55:31 +0200

    FEATURE: Rewrite NodeType::getName()

---
+++ src/ContentRepository90/Rules/FusionNodeTypeNameRector.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\Rector\ContentRepository90\Rules;
+
+use Neos\Rector\Core\FusionProcessing\EelExpressionTransformer;
+use Neos\Rector\Core\FusionProcessing\FusionRectorInterface;
+use Neos\Rector\Utility\CodeSampleLoader;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class FusionNodeTypeNameRector implements FusionRectorInterface
+{
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return CodeSampleLoader::fromFile('Fusion: Rewrite node.nodeType.name to node.nodeTypeName.value', __CLASS__);
+    }
+
+    public function refactorFileContent(string $fileContent): string
+    {
+        return EelExpressionTransformer::parse($fileContent)
+            ->process(fn(string $eelExpression) => preg_replace(
+                '/(node|documentNode|site)\.nodeType\.name/',
+                '$1.nodeTypeName.value',
+                $eelExpression
+            ))
+            ->addCommentsIfRegexMatches(
+                '/\.nodeType.name/',
+                '// TODO 9.0 migration: Line %LINE: You may need to rewrite "VARIABLE.nodeType.name" to VARIABLE.nodeTypeName.value. We did not auto-apply this migration because we cannot be sure whether the variable is a Node.'
+            )->getProcessedContent();
+    }
+}
