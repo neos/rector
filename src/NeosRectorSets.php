cfe0e074cd322425269a6c1152785eaedfb93ef1
Denny Lubitz <lubitz@vivomedia.de>
2023-09-30 09:51:12 +0200

    TASK: Change config path back to set

--- src/NeosRectorSets.php
+++ src/NeosRectorSets.php
@@ -7,7 +7,7 @@ use Rector\Set\Contract\SetListInterface;
 
 class NeosRectorSets implements SetListInterface
 {
-    public const CONTENTREPOSITORY_9_0 = __DIR__ . '/../config/sets/contentrepository-90.php';
+    public const CONTENTREPOSITORY_9_0 = __DIR__ . '/../config/set/contentrepository-90.php';
 
-    public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../config/sets/flow-annotations-to-attributes.php';
+    public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../config/set/flow-annotations-to-attributes.php';
 }
