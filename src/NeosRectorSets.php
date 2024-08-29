5e4b5fa5afbde528a69ab78e397359fe5f62cded
Denny Lubitz <lubitz@vivomedia.de>
2024-08-29 18:33:03 +0200

    TASK: Add Neos 8.4 configuration and test setup

--- src/NeosRectorSets.php
+++ src/NeosRectorSets.php
@@ -8,6 +8,7 @@ use Rector\Set\Contract\SetListInterface;
 class NeosRectorSets implements SetListInterface
 {
     public const CONTENTREPOSITORY_9_0 = __DIR__ . '/../config/set/contentrepository-90.php';
+    public const NEOS_8_4 = __DIR__ . '/../config/set/neos-84.php';
 
     public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../config/set/flow-annotations-to-attributes.php';
 }
