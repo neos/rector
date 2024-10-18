677df861ec88dcb6ca231bab4ee2a8741bf66d97
Denny Lubitz <lubitz@vivomedia.de>
2024-10-18 19:24:27 +0200

    TASK: Add migration for getLabel with NodeLabelGeneratorRector

---
+++ src/ContentRepository90/Rules/NodeLabelGeneratorRector.php
@@ -0,0 +1,60 @@
+<?php
+
+declare (strict_types=1);
+namespace Neos\Rector\ContentRepository90\Rules;
+
+use Neos\Rector\ContentRepository90\Legacy\NodeLegacyStub;
+use Neos\Rector\Utility\CodeSampleLoader;
+use PhpParser\Node;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\Collector\NodesToAddCollector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class NodeLabelGeneratorRector extends AbstractRector
+{
+    use AllTraits;
+
+    public function __construct(
+        private readonly NodesToAddCollector $nodesToAddCollector
+    )
+    {
+    }
+
+    public function getRuleDefinition() : RuleDefinition
+    {
+        return CodeSampleLoader::fromFile('"$node->getLabel()" will be rewritten.', __CLASS__);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes() : array
+    {
+        return [Node\Expr\MethodCall::class];
+    }
+    /**
+     * @param Node\Expr\MethodCall $node
+     */
+    public function refactor(Node $node) : ?Node
+    {
+        assert($node instanceof Node\Expr\MethodCall);
+
+        if (!$this->isObjectType($node->var, new ObjectType(NodeLegacyStub::class))) {
+            return null;
+        }
+
+        if (!$this->isName($node->name, 'getLabel')) {
+            return null;
+        }
+
+        return $this->nodeFactory->createMethodCall(
+            $this->nodeFactory->createPropertyFetch(
+                'this',
+                'nodeLabelGenerator'
+            ),
+            'getLabel',
+            [$node->var]
+        );
+    }
+}
