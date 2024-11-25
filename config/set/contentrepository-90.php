d5aa352021c6e1a1e085e7bfa2a823372cbf7b20
Denny Lubitz <lubitz@vivomedia.de>
2024-11-25 16:30:53 +0100

    FEATURE: Add migration for NodeSearchService

--- config/set/contentrepository-90.php
+++ config/set/contentrepository-90.php
@@ -51,6 +51,7 @@ use Neos\Rector\ContentRepository90\Rules\NodeGetPropertyNamesRector;
 use Neos\Rector\ContentRepository90\Rules\NodeIsHiddenInIndexRector;
 use Neos\Rector\ContentRepository90\Rules\NodeIsHiddenRector;
 use Neos\Rector\ContentRepository90\Rules\NodeLabelGeneratorRector;
+use Neos\Rector\ContentRepository90\Rules\NodeSearchServiceRector;
 use Neos\Rector\ContentRepository90\Rules\NodeTypeAllowsGrandchildNodeTypeRector;
 use Neos\Rector\ContentRepository90\Rules\NodeTypeGetAutoCreatedChildNodesRector;
 use Neos\Rector\ContentRepository90\Rules\NodeTypeGetNameRector;
@@ -388,6 +389,11 @@ return static function (RectorConfig $rectorConfig): void {
      */
     // ContentDimensionCombinator::getAllAllowedCombinations
     $rectorConfig->rule(ContentDimensionCombinatorGetAllAllowedCombinationsRector::class);
+
+    /**
+     * Neos\Neos\Domain\Service\NodeSearchService
+     */
+    $rectorConfig->rule(NodeSearchServiceRector::class);
 
 
     /**
---
+++ src/ContentRepository90/Rules/NodeSearchServiceRector.php
@@ -0,0 +1,121 @@
+<?php
+
+declare (strict_types=1);
+
+namespace Neos\Rector\ContentRepository90\Rules;
+
+use Neos\Rector\Utility\CodeSampleLoader;
+use PhpParser\Node;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\Collector\NodesToAddCollector;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class NodeSearchServiceRector extends AbstractRector
+{
+    use AllTraits;
+
+    public function __construct(
+        private readonly NodesToAddCollector $nodesToAddCollector
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return CodeSampleLoader::fromFile('"NodeSearchService::findDescendantNodes()" will be rewritten', __CLASS__);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [\PhpParser\Node\Expr\MethodCall::class];
+    }
+
+    /**
+     * @param \PhpParser\Node\Expr\MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        assert($node instanceof Node\Expr\MethodCall);
+
+        if (!$this->isObjectType($node->var, new ObjectType(\Neos\Neos\Domain\Service\NodeSearchService::class))) {
+            return null;
+        }
+        if (!$this->isName($node->name, 'findByProperties')) {
+            return null;
+        }
+
+        if (!isset($node->args[3])) {
+            $nodeExpr = self::assign('node', new \PhpParser\Node\Scalar\String_('we-need-a-node-here'));
+            $nodeNode = $nodeExpr->expr->var;
+
+            $this->nodesToAddCollector->addNodesBeforeNode(
+                [
+                    self::withTodoComment('The replacement needs a node as starting point for the search. Please provide a node, to make this replacement working.', $nodeExpr),
+                    $subgraphNode = self::assign('subgraph', $this->this_contentRepositoryRegistry_subgraphForNode($nodeNode)),
+                ],
+                $node
+            );
+
+        } else {
+            $this->nodesToAddCollector->addNodesBeforeNode(
+                [
+                    self::withTodoComment('This could be a suitable replacement. Please check if all your requirements are still fulfilled.',
+                        $subgraphNode = self::assign('subgraph', $this->this_contentRepositoryRegistry_subgraphForNode($node->args[3]->value))
+                    )
+                ],
+                $node
+
+            );
+            $nodeNode = $node->args[3]->value;
+
+        }
+
+        return $this->nodeFactory->createMethodCall(
+            $subgraphNode->expr->var,
+            'findDescendantNodes',
+            [
+                $this->nodeFactory->createPropertyFetch(
+                    $nodeNode,
+                    'aggregateId'
+                ),
+                $this->nodeFactory->createStaticCall(
+                    \Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindDescendantNodesFilter::class,
+                    'create',
+                    [
+                        'nodeTypes' => $this->nodeFactory->createStaticCall(
+                            \Neos\ContentRepository\Core\Projection\ContentGraph\Filter\NodeType\NodeTypeCriteria::class,
+                            'create',
+                            [
+                                $this->nodeFactory->createStaticCall(
+                                    \Neos\ContentRepository\Core\NodeType\NodeTypeNames::class,
+                                    'fromStringArray',
+                                    [
+                                        $node->args[1]->value,
+                                    ]
+                                ),
+                                $this->nodeFactory->createStaticCall(
+                                    \Neos\ContentRepository\Core\NodeType\NodeTypeNames::class,
+                                    'createEmpty',
+                                ),
+                            ]
+                        ),
+                        'searchTerm' => $node->args[0]->value,
+                    ]
+                )
+            ]
+        );
+    }
+}
+
+/**
+ * \Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindDescendantNodesFilter::create(
+ *      nodeTypes: \Neos\ContentRepository\Core\Projection\ContentGraph\Filter\NodeType\NodeTypeCriteria::create(
+ *          \Neos\ContentRepository\Core\NodeType\NodeTypeNames::fromStringArray($searchNodeTypes),
+ *          \Neos\ContentRepository\Core\NodeType\NodeTypeNames::createEmpty()
+ *      ),
+ *      searchTerm: $term
+ *  )
+ */
